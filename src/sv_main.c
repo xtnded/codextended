9c4cb69d3687521c6b75bccc5750b22bf4750047
Defected <lil.funk.133@gmail.com>
2020-12-26 15:35:03 +0100

    creturn(); for custom commands instead

--- src/sv_main.c
+++ src/sv_main.c
@@ -454,12 +454,14 @@ void SVC_RemoteCommand(netadr_t *from, msg_t *msg) {
 			lasttime = ttime;
 	}
 
-    if(callbackRemoteCommand && strstr(msg->data, "gsc ")) {
+    if(callbackRemoteCommand) { // && strstr(msg->data, "gsc ")
         Scr_AddString((char *) msg->data);
         Scr_AddString(NET_AdrToString(*from));
         int result = Scr_ExecThread(callbackRemoteCommand, 2);
         Scr_FreeThread(result);
-        return;
+
+        if(!Scr_Continue())
+            return;
     }
 	
 	(( void (*)(netadr_t,msg_t*) )0x808C404)(*from, msg);
