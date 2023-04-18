855df4fb01d20f19091d18d46980b5fdfa95a712
raphael12333 <130972077+raphael12333@users.noreply.github.com>
2023-04-18 19:15:18 +0200

    new feature get unixtime in gsc (#15)
    intended usage for temporary banning

--- src/script.h
+++ src/script.h
@@ -368,6 +368,7 @@ void GScr_salt_password(int a1);
 void GScr_getconfigstring(int);
 void GScr_configstringindex(int);
 void GScr_system(int);
+void GScr_getUnixTime();
 void GScr_strpos(int);
 
 void GScr_SpawnAnimEnt(int);
